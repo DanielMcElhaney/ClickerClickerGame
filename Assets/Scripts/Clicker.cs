b5d6518c80c0a6c8073da40d635aca998ad18a83
DanielMcElhaney <daniel.mcelhaney@gmail.com>
2021-01-21 12:57:49 -0600

    clicks Per Second Counter

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -7,13 +7,16 @@ public class Clicker : MonoBehaviour
     public Text coinsText;
     public Text clickerButtonText;
     public Text upgradeButtonText; 
-    public Text upgradeText;
     public Text automationButtonText;  
     public Text powerButtonText;
+    public Text upgradeText;
+    public Text clicksPerSecond;
+
     public Button UpgradeButton;
     public Button ClickerButton; 
     public Button AutomationButton;
     public Button PowerButton;
+
     public Data coinData;
     
    
@@ -37,8 +40,11 @@ public class Clicker : MonoBehaviour
         upgradeButtonText.text = coinData.upgradeAmt.ToString("F0") + " Coins";
         //Update Automation Button Text
         automationButtonText.text = coinData.automationAmt.ToString("F0") + " Coins";
+        //Update Clicks Per Second Text
+        clicksPerSecond.text = "Clicks Per Second: " + coinData.automationClickCount;
         //Update Power Button Text
         powerButtonText.text = coinData.powerAmt.ToString("F0") + " Coins";
+
         
 
         //Activates Upgrade Button
@@ -56,36 +62,59 @@ public class Clicker : MonoBehaviour
         //Runs the clicks per second
         if(coinData.automationClickCount>0)
         {
-        coinData.coins += (coinData.automationClickCount * Time.deltaTime);
-        coinData.totalCoins += (coinData.automationClickCount * Time.deltaTime);
+        coinData.coins += (coinData.automationClickCount * coinData.coinMulti * Time.deltaTime);
+        coinData.totalCoins += (coinData.automationClickCount * coinData.coinMulti * Time.deltaTime);
         }
+
     }
 
     public void Click()
     {
-         coinData.coins += (1*coinData.coinMulti);
-         coinData.totalCoins += (1*coinData.coinMulti);
+        //Adds one coin + the current multiplier
+         coinData.coins += (1f*coinData.coinMulti);
+         coinData.totalCoins += (1f*coinData.coinMulti);
     }
 
     public void UpgradeClick()
     {
-        coinData.coinMulti += 1*coinData.powerClickCount;
+        //Pays for upgrade
         coinData.coins -= coinData.upgradeAmt;
-        coinData.upgradeClickCount +=1;
-        coinData.upgradeAmt*=Random.Range(1.07f,1.15f);
+
+        //Increases power of the multiplier by 1 times the power modifier
+        coinData.coinMulti += 1f*coinData.powerPower;
+        
+        //Adds 1 to the total upgrades
+        coinData.upgradeClickCount +=1f;
+
+        //Increases cost of next upgrade.
+        coinData.upgradeAmt*=coinData.growthMulti;
     }
 
     public void AutomationClick()
     {
+        //Pays for automator
         coinData.coins -= coinData.automationAmt;
-        coinData.automationClickCount +=1;
-        coinData.automationAmt *= Random.Range(1.07f,1.15f);
+
+        //Add 1 to count of automator purchases
+        coinData.automationClickCount +=1f;
+
+        //Increased cost of next automator
+        coinData.automationAmt *= coinData.growthMulti;
     }  
   
     public void ClickPower()
-    {
+    {   
+        //Pays for power upgrade
+        coinData.coins -= coinData.powerAmt;
+
+        //Adds one to number of power upgrades
         coinData.powerClickCount++;
-        coinData.powerAmt *= Random.Range(1.07f,1.15f);
+
+        //Increases cost of next power upgrade
+        coinData.powerAmt *= coinData.growthMulti;
+
+        //Increases the power of the upgrade
+        coinData.powerPower++;
     }
    
 }
--- Assets/Scripts/CoinData.cs
+++ Assets/Scripts/CoinData.cs
@@ -12,18 +12,33 @@ public class Data
     public double automationClickCount;
     public double automationAmt;
     public double powerAmt;
+    public double powerPower;
     public double powerClickCount;
+    public double growthMulti;
 
     public Data()
     {
-        coins = 0;
-        totalCoins = 0;
-        upgradeAmt = 10;
-        coinMulti = 1;
-        upgradeClickCount = 1;
-        automationClickCount = 0;
-        automationAmt = 1000;
-        powerClickCount = 1;
-        powerAmt = 5000;
+        //Current Coins
+        coins = 0f;
+        //Lifetime Total Coins
+        totalCoins = 0f;
+        //Upgrade Cost
+        upgradeAmt = 10f;
+        //Coins per Click
+        coinMulti = 1f;
+        //Number of Upgrades
+        upgradeClickCount = 1f;
+        //Number of Automators
+        automationClickCount = 0f;
+        //Cost of Automators
+        automationAmt = 250f;
+        //Number of Power Upgrades
+        powerClickCount = 1f;
+        //Cost of Power Upgrade
+        powerAmt = 5000f;
+        //Power of Power Upgrade
+        powerPower = 1f;
+        //Growth Rate of Costs
+        growthMulti = 1.2f;
     }
 }
